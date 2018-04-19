6a60534d0d3d00ac925cf2f8f2f568d9df31ad9b
Nakanishi Tetsuhiro <kaoru6string@gmail.com>
2018-04-19 17:21:16 +0900

    Lesson 05

--- js/main.js
+++ js/main.js
@@ -6,11 +6,22 @@
     var vm = new Vue({
         el: '#app',
         data: {
+            newItem: '',
             todos: [
                 'task1',
                 'task2',
                 'task3'
             ]
+        },
+        methods: {
+            // // formでsubmitするのをpreventDefault()で抑制できるぜ！
+            // addItem: function(e) {
+            //     e.preventDefault();
+            //     this.todos.push(this.newItem)
+            // けど、htmlの方で@submit.preventってしたら上のコードと同じ意味になる
+            addItem: function() {
+                this.todos.push(this.newItem)
+            }
         }
     })
 }
